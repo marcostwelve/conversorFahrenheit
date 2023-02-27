0fe537ef6950fa5ae6b28dd4165b29bb911f7491
marcostwelve <marcosnyan12@gmail.com>
2023-02-27 20:53:44 -0300

    Update 1.0

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-
 // Método para cálculo
-document.getElementById("btn_calculo").onclick = function() {
-    var celsius = document.getElementById("icel").value;
-    if(celsius == "") {
-        alert("Você precisa preencher o campo de Celsius (°C)!");
+
+// document.getElementById("btn_calculo").onclick = function() {
+//     var celsius = document.getElementById("icel").value;
+//     if(celsius == "") {
+//         alert("Você precisa preencher o campo de Celsius (°C)!");
+//     }
+
+//     else {
+//         var fahrenheit = ((celsius * 1.8) + 32);
+//         document.getElementById("ifah").value = fahrenheit+"°F";
+//     }
+// }
+const celsius = document.getElementById("icel");
+const fahrenheit = document.getElementById("ifah");
+
+window.addEventListener("load", () => celsius.focus());
+
+celsius.addEventListener("input", () => {
+    fahrenheit.value = ((celsius.value * 1.8) + 32).toFixed(2)+"°F";
+    if(!celsius.value) {
+        fahrenheit.value = "";
     }
-    else {
-        var comparacao = document.getElementById("comparacao");
-        comparacao.style.display = "block";
-        var fahrenheit = ((celsius * 1.8) + 32);
-        document.getElementById("fah").innerHTML = `
-            <div id=fah>
-                <label for="ifah">°F:</label>
-                <input type="text" id="ifah"" value="${fahrenheit.toFixed(1)}°F"/>
-            </div>    
-        `
-    }
-}
+});
 
 // Método para limpar campos
 document.getElementById("btn_limpar").onclick = function() {
-   var comparacao = document.getElementById("comparacao");
-   comparacao.style.display = "none"; 
-   var celsius = document.getElementById("icel");
-   var fahrenheit = document.getElementById("fah");
-   celsius.value = "";
-   fahrenheit.innerHTML = "";
+   document.getElementById("icel").value = 0;
+   document.getElementById("ifah").value = "";
 }
